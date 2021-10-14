cd72fc9a1f327faac57adaa80b2027a56a082da0
Ievgiesha <frosia1973@gmail.com>
2021-10-14 12:45:30 +0200

    Added some Assertion Methods to AlertsTests class.

--- src/main/java/com/herokuapp/theinternet/alertstests/AlertsTests.java
+++ src/main/java/com/herokuapp/theinternet/alertstests/AlertsTests.java
@@ -3,12 +3,13 @@ package com.herokuapp.theinternet.alertstests;
 import com.herokuapp.theinternet.base.TestUtilities;
 import com.herokuapp.theinternet.pages.JavaScriptAlertsPage;
 import com.herokuapp.theinternet.pages.WelcomePageObject;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
 public class AlertsTests extends TestUtilities {
     @Test
-    public void jsAlertTest(){
+    public void jsAlertTest() {
         log.info("Starting jsAlertTest");
 
         // open main page
@@ -17,6 +18,27 @@ public class AlertsTests extends TestUtilities {
 
         //Click on JS Alert Link
         JavaScriptAlertsPage alertsPage = welcomePage.clickJavaScriptAlertsLink();
+
+        //Click JS Prompt Button
+        alertsPage.openJSPrompt();
+
+        // Get Alert Text
+        String alertMessage = alertsPage.getAlertText();
+
+        //Type text into alert
+        alertsPage.typeTextIntoAlert("Hello Alert, It's Ievgeniia here.");
+
+        //Get result text
+        String result = alertsPage.getResultText();
+
+        //Verification
+        // 1 - Alert text is expected
+        Assert.assertTrue(alertMessage.equals("I am a JS prompt"),
+                "Alert message is not expected. \nShould be 'I am a JS prompt' ");
+
+        //Result text is expected
+        Assert.assertTrue(result.equals("You entered: Hello Alert, It's Ievgeniia here."),
+                " Result message is not expected. \nShould be: 'It's Ievgeniia here.'");
 
 
     }
