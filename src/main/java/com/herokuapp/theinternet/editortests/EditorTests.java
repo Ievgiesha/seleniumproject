e5b1f31fddac55f007f9732ff37877f26b042b0c
Ievgiesha <frosia1973@gmail.com>
2021-10-15 11:47:02 +0200

    Added Key Presses Page Tests

--- src/main/java/com/herokuapp/theinternet/editortests/EditorTests.java
+++ src/main/java/com/herokuapp/theinternet/editortests/EditorTests.java
@@ -16,15 +16,16 @@ public class EditorTests extends TestUtilities {
         // open main page
         WelcomePageObject welcomePage = new WelcomePageObject(driver, log);
         welcomePage.openPage();
-
+        sleep(2000);
         // Click on WYSIWYG Editor link
         EditorPage editorPage = welcomePage.clickWYSIWYGEditorLink();
-
+        sleep(2000);
         // Get default editor text
         String editorText = editorPage.getEditorText();
-
+        sleep(2000);
         // Verification that new page contains expected text in source
         Assert.assertTrue(editorText.equals("Your content goes here."),
                 "Editor default text is not expected. It is: " + editorText);
+        sleep(2000);
     }
 }
