8c4385f4c265816720d4390189d847d4e4e2752d
Ievgiesha <frosia1973@gmail.com>
2021-10-19 18:32:42 +0200

    Modify method imadeUploaderPage to FileUploaderPage with annotation dataProvider and added method files to TestUtilites

--- src/main/java/com/herokuapp/theinternet/base/TestUtilities.java
+++ src/main/java/com/herokuapp/theinternet/base/TestUtilities.java
@@ -1,6 +1,7 @@
 package com.herokuapp.theinternet.base;
 
 import com.herokuapp.theinternet.base.BaseTest;
+import org.testng.annotations.DataProvider;
 
 public class TestUtilities extends BaseTest {
 
@@ -11,5 +12,14 @@ public class TestUtilities extends BaseTest {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+	}
+
+	@DataProvider(name="files")
+	protected static Object[][] files(){
+		return new Object[][]{
+				{1,"index.html"},
+				{2, "logo.png"},
+				{3, "text.txt"}
+		};
 	}
 }
